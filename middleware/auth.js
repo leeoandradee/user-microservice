4935552a564ad1e7d9ee06c99d0ecde0cae80008
Leonardo Andrade <leoandrade.developer@gmail.com>
2021-11-28 12:29:40 -0300

    melhorando validacoa de token

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,24 @@ const jwt = require("jsonwebtoken");
 const conf = require("../config/config");
 
 const auth = (req, res, next) => {
-  let token_created = req.headers.authorization;
-  token_created = token_created.split(' ')[1];
+  const token_created = req.headers.authorization;
 
   if (!token_created) {
-    return res.status(401).send({ output: `Access denied` });
+    return res.status(401).send({ output: `token não informado` });
   }
 
+  const parts = token_created.split(" ");
+
+  if (!parts.length === 2) {
+    return res.status(401).send({ error: "erro no token" });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).send({ error: "token mal formado" });
+  }
+  
   jwt.verify(token_created, conf.jwt_key, (error, data) => {
     if (error) {
       return res.status(401).send({ output: `Token fail ->${error}` });
